cf47ec4f83e32e7d8fa527f19a536a12d72773c0
Andrei Kaminsky <andrewmen2008@gmail.com>
2021-11-08 20:10:16 +0300

    новый комментарий к новой ветке

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World  new!");
+        System.out.println("Hello World  new2!");
+        System.out.println("Hello World  new2!");
     }
 }
